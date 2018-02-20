26424d56d5a96b3e407270955b3ac61a998aa340
Doug <douglas.hoelzel@gmail.com>
2018-02-20 11:55:30 -0500

    added ajax call to work on in MapPage

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -11,21 +11,40 @@ import {
 import { WebBrowser } from 'expo';
 import { MapView } from 'expo';
 
-
 export default class MapScreen extends React.Component {
+constructor(props){
+    super(props);
+    this.state = {
+        rovers: [],
+        data: []
+    }
+}
+
+
   static navigationOptions = {
     header: null,
   };
   
+  componentWillMount(){
+        this.fetchAllLocations();
+        this.getRovers();
+        
+  }
+  getRovers = async () =>{
+      var url = "https://nodejs-mongo-persistent-nmchenry.cloudapps.unc.edu/api/alllocations";
+      return fetch(url).then((res) => res.json());
+      this.setState({ rovers: res.rovers});
+      console.log("inside of rovers function");
+  };
   fetchAllLocations = async () => {
         const response = await fetch("https://nodejs-mongo-persistent-nmchenry.cloudapps.unc.edu/api/alllocations");
         const json = await response.json();
         this.setState({ data: json });
-        console.log("AJAX RETURNS: " , json);
     };
 
   render() {
-      console.log("inside of render function");
+      console.log("Data[]: " , this.state.data);
+      console.log("Rovers[]: " , this.state.rovers);
     return (
       <View style={styles.container}>
             <Text style={styles.getStartedText}>
@@ -39,7 +58,18 @@ export default class MapScreen extends React.Component {
                   latitudeDelta: 1,
                   longitudeDelta: 1,
                 }}
-              />
+                />
+                  {/*{this.state.data.map(marker => (
+                  <Marker 
+                  coordinate={{
+                  latitude: marker.latitude,
+                  longitude:  marker.longitude}}
+                  title={marker.title}
+                  description={marker.description}
+                  onCalloutPress={this.toggleModal}
+                  />
+                  ))}
+              </MapView>*/}
       </View>
     );
   }
