02a69d3a976d507af0c6ed4329a03572baffa3d5
averylue <avatarlue@gmail.com>
2018-02-25 15:34:06 -0500

    Added alerts to notify user of success/failure, minor adjustments

--- screens/AddLocationScreen.js
+++ screens/AddLocationScreen.js
@@ -38,7 +38,9 @@ export default class AddLocation extends React.Component {
     header: null,
   };
 
+//Takes input from form, sends to api
   submitPress = (location, desc, lat, long) => {
+    //Check if parts of form are empty, alert user to complete form.
       if(!location || !desc || !lat || !long){
         console.log("Please fill out entire form before submitting.")
         Alert.alert(
@@ -50,6 +52,7 @@ export default class AddLocation extends React.Component {
         )
       }
       else{
+        //If form is complete, send post to api
         const url = "https://nodejs-mongo-persistent-nmchenry.cloudapps.unc.edu/api/addlocation";
         fetch(url, {
               method: 'POST',
@@ -63,29 +66,77 @@ export default class AddLocation extends React.Component {
                 latitude: lat,
                 longitude: long,
               })
-        }).then(response => console.log(response));
+        }).then(response => {
+          //if response is 200 (success), alert user and log response
+          if(response.status === 200) {
+            Alert.alert(
+              'SUBMISSION SUCCESSFUL',
+              'Thank you for your submission!',
+              [
+                {text: 'OK', onPress: () => console.log('OK pressed')},
+              ],
+            )
+              console.log("The Submission Was Successful");
+          }
+          //if response is status 400 (key not unique), alert user location exists and log response
+          else if(response.status === 400){
+            Alert.alert(
+              'SUBMISSION FAILED',
+              'A location with this name has already been added!',
+              [
+                {text: 'OK', onPress: () => console.log('OK pressed')},
+              ],
+            )
+              console.log("An Error Has Occurred: Duplicate Key Error");
+
+          }
+          //if any other response is received, alert user something went wrong and log response
+          else{
+            Alert.alert(
+              'SUBMISSION FAILED',
+              'Unfortunately, we cannot accept this submission at this time.',
+              [
+                {text: 'OK', onPress: () => console.log('OK pressed')},
+              ],
+            )
+              console.log("An Unknown Error Has Occurred");
+          }
+          console.log("Response status: " + response.status)
+
+        }).catch(err => {
+          //for if something goes wrong contacting the api
+          Alert.alert(
+            'UNABLE TO SUBMIT',
+            'Something is preventing submission. Please try again later.',
+            [
+              {text: 'OK', onPress: () => console.log('OK pressed')},
+            ],
+          )
+          console.log(err);
+        });
 
       }
   }
 
-  testSubmit = (location, desc, lat, long) => {
-    if(!location || !desc || !lat || !long){
-      console.log("Please Complete All Parts Of Form");
-    }
-    else{
-      console.log(location, desc, lat, long);
-    }
-  }
+//the following code was used to test the form before testing it with the api
+//It is no longer necessary, but left here for documentation purposes.
+  // testSubmit = (location, desc, lat, long) => {
+  //   if(!location || !desc || !lat || !long){
+  //     console.log("Please Complete All Parts Of Form");
+  //   }
+  //   else{
+  //     console.log(location, desc, lat, long);
+  //   }
+  // }
 
 
   render() {
     return (
-      //The following is Avery testing native-base for form building
+      //The following is native-base to create the form itself, written by Avery
       <Container style={styles.container}>
-        <Header><Text>Add Location Form</Text></Header>
+        <Header><Text style={styles.getStartedText}>Add Location Form</Text></Header>
         <Content>
           <Form>
-
 
             <Item style={styles.formText}>
               <Label style={styles.label}>Title: </Label>
@@ -131,16 +182,19 @@ export default class AddLocation extends React.Component {
                 this.state.lat,
                 this.state.long
               )}>
-                <Text>Submit</Text>
+                <Text style={styles.buttonText}>SUBMIT</Text>
             </Button>
 
           </Form>
         </Content>
       </Container>
 
+    );
+  }
+}
 
-
-      //The Below is Old Code, I'm testing above
+      //Comment code below is the previous AddLocation.js code, current code is above.
+      //This is left here for documentation purposes only.
 
       // <View style={styles.container}>
       //       <Text style={styles.getStartedText}>
@@ -150,11 +204,6 @@ export default class AddLocation extends React.Component {
       //         Currently being edited by Avery
       //       </Text>
       //     </View>
-
-
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -166,6 +215,11 @@ const styles = StyleSheet.create({
   },
   submitButton: {
     alignSelf: 'center',
+    marginTop: 30,
+    height: 50,
+    width: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   formText: {
     alignSelf: 'center',
@@ -176,8 +230,14 @@ const styles = StyleSheet.create({
   getStartedText: {
     fontSize: 20,
     color: 'rgba(96,100,109, 1)',
-    marginTop: 30,
+    marginTop: 20,
     lineHeight: 24,
-
+    textAlign: 'center',
+  },
+  buttonText: {
+    fontSize: 15,
+    color: '#fff',
+    lineHeight: 24,
+    textAlign: 'center',
   }
 });
