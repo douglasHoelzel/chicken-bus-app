17ce60c4814b62e5c8b41bc9e330105ffc4d87b3
KTRAN19-PC\ktran19 <kennytran8310@gmail.com>
2018-03-18 11:55:58 -0400

    added facebook and google login

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -48,6 +48,102 @@ componentWillMount(){
     }
     firebase.initializeApp(firebaseConfig);
 }
+loginWithGoogle = async() => {
+  const result = await Expo.Google.logInAsync({
+    androidClientId: '396242534921-ql4pfkgkonbqe357ed9vn3tm2hi5ihoo.apps.googleusercontent.com',
+    iosClientId: '396242534921-j13r4u4pfenrqpdmgq0ekgfesuhcrg6d.apps.googleusercontent.com',
+    scopes: ['profile', 'email'],
+  });
+
+  if (result.type === 'success') {
+
+    // Build Firebase credential with the Facebook access token.
+    const credential = firebase.auth.GoogleAuthProvider.credential(result.idToken, result.accessToken);
+
+    // Sign in with credential from the Facebook user.
+    this.setState({loading: true});
+
+    firebase.auth().signInWithCredential(credential)
+    .then((user) => {
+      console.log("New User ID: " + JSON.stringify(user));
+      GLOBAL.USERID = user.uid;
+      GLOBAL.ISLOGGEDIN = true;
+      GLOBAL.EMAIL = user.email;
+      console.log("Email: " + user.email);
+      this.getUserInfo(user.uid);
+        this.setState({loading: false, isLoggedIn: true, userID: user.uid});
+        {/* Sends New User Information to Database*/}
+        const url = "https://nodejs-mongo-persistent-nmchenry.cloudapps.unc.edu/api/adduser";
+        console.log("UserID: " + GLOBAL.USERID + " USERNAME: " + GLOBAL.USERNAME );
+        fetch(url, {
+               method: 'POST',
+               headers: {
+                 Accept: 'application/json',
+                 'Content-Type': 'application/json',
+               },
+               body: JSON.stringify({
+                 userID: GLOBAL.USERID,
+                 username: GLOBAL.USERNAME,
+               })
+          });
+      })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("SIGN UP ERROR CODE: " + errorCode + " SIGN UP ERROR MESSAGE: " + errorMessage);
+      Alert.alert(errorMessage);
+      this.setState({loading: false, isLoggedIn: false, password: '', email: ''});
+    });
+  }
+}
+
+loginWithFacebook = async() => {
+  const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
+    '201285720459409',
+    { permissions: ['public_profile', 'email'] }
+  );
+
+  if (type === 'success') {
+    // Build Firebase credential with the Facebook access token.
+    const credential = firebase.auth.FacebookAuthProvider.credential(token);
+
+    // Sign in with credential from the Facebook user.
+    this.setState({loading: true});
+
+    firebase.auth().signInWithCredential(credential)
+    .then((user) => {
+      console.log("New User ID: " + JSON.stringify(user));
+      GLOBAL.USERID = user.uid;
+      GLOBAL.ISLOGGEDIN = true;
+      GLOBAL.EMAIL = user.email;
+      console.log("Email: " + user.email);
+      this.getUserInfo(user.uid);
+        this.setState({loading: false, isLoggedIn: true, userID: user.uid});
+        {/* Sends New User Information to Database*/}
+        const url = "https://nodejs-mongo-persistent-nmchenry.cloudapps.unc.edu/api/adduser";
+        console.log("UserID: " + GLOBAL.USERID + " USERNAME: " + GLOBAL.USERNAME );
+        fetch(url, {
+               method: 'POST',
+               headers: {
+                 Accept: 'application/json',
+                 'Content-Type': 'application/json',
+               },
+               body: JSON.stringify({
+                 userID: GLOBAL.USERID,
+                 username: GLOBAL.USERNAME,
+               })
+          });
+      })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("SIGN UP ERROR CODE: " + errorCode + " SIGN UP ERROR MESSAGE: " + errorMessage);
+      Alert.alert(errorMessage);
+      this.setState({loading: false, isLoggedIn: false, password: '', email: ''});
+    });
+  }
+}
+
 onEmailSignInPress = (email, password) => {
     console.log("Existing user signing in");
     this.setState({loading: true});
@@ -60,6 +156,20 @@ onEmailSignInPress = (email, password) => {
         console.log("Email: " + user.email);
         this.getUserInfo(user.uid);
         this.setState({loading: false, isLoggedIn: true, userID: user.uid});
+        {/* Sends New User Information to Database*/}
+        const url = "https://nodejs-mongo-persistent-nmchenry.cloudapps.unc.edu/api/adduser";
+        console.log("UserID: " + GLOBAL.USERID + " USERNAME: " + GLOBAL.USERNAME );
+        fetch(url, {
+               method: 'POST',
+               headers: {
+                 Accept: 'application/json',
+                 'Content-Type': 'application/json',
+               },
+               body: JSON.stringify({
+                 userID: GLOBAL.USERID,
+                 username: GLOBAL.USERNAME,
+               })
+          });
       })
     .catch((error) =>  {
         var errorCode = error.code;
@@ -67,6 +177,7 @@ onEmailSignInPress = (email, password) => {
         console.log("SIGN UP ERROR CODE: " + errorCode + " SIGN UP ERROR MESSAGE: " + errorMessage);
         Alert.alert(errorMessage);
         this.setState({loading: false, isLoggedIn: false, password: '', email: ''});
+
     })
 };
 
@@ -207,6 +318,26 @@ renderCurrentState(){
                         onPress={() => this.toggleSignUpModal()}
                     >
                     <Text style={styles.buttonText}> Create Account </Text>
+                    </TouchableOpacity>
+            </View>
+            {/*Create Google Button*/}
+            <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={styles.buttonCell}
+                        onPress={() => this.loginWithGoogle()}
+
+                    >
+                    <Text style={styles.buttonText}> Sign In With Google </Text>
+                    </TouchableOpacity>
+            </View>
+            {/*Create Facebook Button*/}
+            <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={styles.buttonCell}
+                        onPress={() => this.loginWithFacebook()}
+
+                    >
+                    <Text style={styles.buttonText}> Sign In With Facebook </Text>
                     </TouchableOpacity>
             </View>
           </ScrollView>
